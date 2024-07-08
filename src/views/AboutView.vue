17ec3345f87ce83569c70b398649d6c82fbe2070
李权 <11483845+kingsleyesovo@user.noreply.gitee.com>
2024-07-09 00:41:18 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <input v-model="inputValue" />
+  {{ data }}
+  <div>{{ count }}</div>
+  <button @click="handleOnclick">按我
+  </button>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import axios from 'axios'
+const count = ref(0)
+const inputValue = ref('')
+const data = ref('')
+const handleOnclick = async () => {
+  data.value = await axios.get("http://localhost:9000/chat")
+    .then((res => res.data))
+    .catch(function (error) {
+      console.log(error)
+    })
+
+}
+
+
+</script>
